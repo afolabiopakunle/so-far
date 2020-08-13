918a2b56813dc651b785dbe444d443447fbcdd13
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-13 14:49:19 +0100

    Added and connected  MongoDB

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 let app = require("express")();
 let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
 let PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-let students = [
-  {
-    name: "Jide",
-    surname: "Kosoko",
-    imageURL: "https://randomuser.me/api/portraits/men/32.jpg"
-  },
-  {
-    name: "Tunde",
-    surname: "Ednut",
-    imageURL: "https://randomuser.me/api/portraits/men/3.jpg"
-  },
-  {
-    name: "Kola",
-    surname: "Oyewo",
-    imageURL: "https://randomuser.me/api/portraits/men/86.jpg"
-  }
-];
+mongoose.connect("mongodb://localhost/students-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const studentSchema = mongoose.Schema({
+  name: String,
+  imageURL: String
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
+// Student.create({
+//   name: "Babajide Sanwolu",
+//   imageURL: "https://uifaces.co/our-content/donated/1H_7AxP0.jpg"
+// });
+
+// let students = [
+//   {
+//     name: "Jide",
+//     surname: "Kosoko",
+//     imageURL: "https://randomuser.me/api/portraits/men/32.jpg"
+//   },
+//   {
+//     name: "Tunde",
+//     surname: "Ednut",
+//     imageURL: "https://randomuser.me/api/portraits/men/3.jpg"
+//   },
+//   {
+//     name: "Kola",
+//     surname: "Oyewo",
+//     imageURL: "https://randomuser.me/api/portraits/men/86.jpg"
+//   }
+// ];
 
 app.get("/", (req, res) => {
-  res.render("all-students", { students });
+  Student.find((err, students) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // console.log(students);
+      res.render("all-students", { students });
+    }
+  });
 });
 
 app.get("/add-student", (req, res) => {
@@ -33,7 +58,11 @@ app.post("/add-student", (req, res) => {
   let name = req.body.name;
   let imageURL = req.body.imageURL;
   let surname = req.body.surname;
-  students.push({ name, imageURL, surname });
+  Student.create({ name, imageURL, surname }, (err, student) => {
+    if (err) {
+      console.log(err);
+    }
+  });
   res.redirect("/");
 });
 
